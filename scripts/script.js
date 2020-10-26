fc6b3dd7d21e75977fe70fa2751626fe2de8f119
Lucas Dordolo <lucasd92@hotmail.com>
2020-10-26 14:38:01 -0300

    Solved exercise 2-f

--- scripts/script.js
+++ scripts/script.js
@@ -81,3 +81,18 @@ var longString = "i'M A lOnG sTrInG";
 var firstSpacePosition = longString.indexOf(' ');
 
 console.log('The first space is in position: ' + firstSpacePosition); 
+
+/* Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún 
+espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un
+nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras
+en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +). */
+
+var longString = "inComprEhenSibIliTies dichloRodifluOromeThaNe";
+
+var spaceIndex = longString.indexOf(' ');
+
+var firstWord = longString.substring(0,1).toUpperCase() + longString.substring(1,spaceIndex).toLowerCase();
+
+var secondWord = longString.substring(spaceIndex + 1, spaceIndex + 2).toUpperCase() + longString.substring(spaceIndex + 2).toLowerCase();                
+
+console.log(firstWord + ' ' + secondWord);
