8537e59c661b97c7697f8cc44b7e400acac286c2
Lucas Dordolo <lucasd92@hotmail.com>
2020-10-26 15:36:09 -0300

    Solved exercise 5-d

--- scripts/script.js
+++ scripts/script.js
@@ -234,3 +234,15 @@ monthsArray.forEach(function(element,index,array){
 });
 
 alert(sentence);
+
+/* Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número
+0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log). */
+
+var newArray = [];
+
+for(var i = 0; i < 10; i++){
+    newArray[i] = i;
+}
+
+console.log(newArray);
