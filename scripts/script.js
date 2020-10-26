a3531e30ed8ae77911600e165e8dffe413a04297
Lucas Dordolo <lucasd92@hotmail.com>
2020-10-26 15:12:44 -0300

    Solved exercise 4-b

--- scripts/script.js
+++ scripts/script.js
@@ -168,3 +168,36 @@ if(randomNumber >= 0.5){
 else{
     alert('Lower than 0,5');
 }
+
+/* Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+    “Bebe” si la edad es menor a 2 años
+    “Nino” si la edad es entre 2 y 12 años
+    “Adolecente” si la edad es entre 13 y 19 años
+    “Joven” si la edad está entre 20 y 30 años
+    “Adulto” entre 31 y 60 años
+    “Adulto mayor” entre 61 y 75 años
+    “Anciano” si es mayor a 75 años */
+
+var randomAge = Math.round(Math.random() * 100);
+
+if(randomAge < 2){
+    alert('Bebe');
+}
+else if(randomAge < 13){
+    alert('Nino');
+}
+else if(randomAge < 20){
+    alert('Adolescente');
+}
+else if(randomAge < 31){
+    alert('Joven');
+}
+else if(randomAge < 61){
+    alert('Adulto');
+}
+else if(randomAge < 76){
+    alert('Adulto mayor');
+}
+else{
+    alert('Anciano');
+}
